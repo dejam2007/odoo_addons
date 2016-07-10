5ea32698cd3a49adf535773076fba84dd926ddc2
dejam2007 <daijinmin@gmail.com>
2016-07-10 23:58:06 +0800

    v0.01

--- dcm/__openerp__.py
+++ dcm/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "dcm",
+    'name': "DCM",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': """"
+        testing bug management
+    """,
 
     'description': """
         Long description of module's purpose
@@ -26,7 +26,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- dcm/models/models.py
+++ dcm/models/models.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
+from odoo.tools import html2plaintext
 
-# class dcm(models.Model):
-#     _name = 'dcm.dcm'
+class Question(models.Model):
+    _name = 'dcm.question'
+    # _inherit = ['mail.thread']
+    _description = "Question"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name       = fields.Text(compute='_compute_name', string='Question Summary', store=True)
+    user_id    = fields.Many2one('res.users', string='Owner', default=lambda self: self.env.uid)
+    memo       = fields.Html('Question Content')
+
+    @api.depends('memo')
+    def _compute_name(self):
+        """ Read the first line of the memo to determine the question name """
+        for question in self:
+            text = html2plaintext(question.memo) if question.memo else ''
+            question.name = text.strip().replace('*', '').split("\n")[0]
